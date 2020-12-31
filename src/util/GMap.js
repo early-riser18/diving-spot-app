d0aba16e346cfb437fefd2964e9b043d3852563e
early-riser18 <justin.warambourg@gmail.com>
2020-12-31 14:41:54 +0100

    t

--- src/util/GMap.js
+++ src/util/GMap.js
@@ -1,25 +1,23 @@
 import React, { Component, useState, UseEffect } from "react";
 import GoogleMapReact from "google-map-react";
-import { LoaderMapSpotFetch } from "../components/LoaderMapSpotFetch/LoaderMapSpotFetch";
-import { SpotPreviewMini } from "../components/SpotPreviewMini/SpotPreviewMini";
-import {MapMarker} from '../components/MapMarker/MapMarker';
 
+const AnyReactComponent = ({ src, idRef }) => <div><a href={`#${idRef}`}><img style={{width: "20px"}} src={src} alt=''/></a></div>;
 
 class GMap extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { isLoaded: false };
+    this.state = {isLoaded: false}
   }
-
+  
   static defaultProps = {
-    isFetching: false,
+    
     zoom: 11,
   };
 
-  componentDidMount() {
-    this.setState({ isLoaded: true });
-  }
+componentDidMount(){
+  this.setState({isLoaded: true});
+}
   validateCenter() {
     let validatedCenter;
 
@@ -32,76 +30,49 @@ class GMap extends Component {
     return validatedCenter;
   }
 
-  loadMarkers(markerSelected) {
+
+
+  loadMarkers() {
     let markersArray = [];
-    if (typeof this.props.queryResult === "object" && this.props.queryResult) {
+
+    if (typeof this.props.queryResult === 'object' && this.props.queryResult) {
       this.props.queryResult.map((val, index) => {
         let resultData = Object.values(val)[0];
         // console.log()
-        let marker = (
-          <MapMarker
-            onClick={this.props.onMarkerClick}
-            key={Object.keys(val)[0]}
-            idRef={Object.keys(val)[0]}
-            lat={resultData.latitude}
-            lng={resultData.longitude}
-            src={require(`../assets/pin-icon-${resultData.level}.png`)}
-          />
-        );
+        let marker = <AnyReactComponent key={Object.keys(val)[0]} idRef={Object.keys(val)[0]} lat={resultData.latitude} lng={resultData.longitude}             src={require(`../assets/pin-icon-${resultData.level}.png`)}
+        />;
         markersArray.push(marker);
-
-        if (markerSelected){
-          let s1 = markerSelected.substring(1);
-        
-          if (s1 === Object.keys(val)[0]) {
-            console.log(s1, Object.values(val)[0]);
-            let x = (
-              <SpotPreviewMini handleClosePreviewMini={this.props.handleClosePreviewMini} markerRefId={Object.keys(val)[0]} lat={resultData.latitude} lng={resultData.longitude} spotData={Object.values(val)[0]} />
-            );
-            markersArray.push(x);
-          }
-        }
       });
     }
-    return markersArray;
-  }
-
-  loadSpotPrevMini(markerSelected) {
-    // if (markerSelected){
-    //   <SpotPreviewMini lat={} lng={} markerPos={} spotID={spotID} />
-    // }
+    return markersArray
   }
 
   createMapOptions(maps) {
     return {
       scrollwheel: true,
-      gestureHandling: "greedy",
-      fullscreenControl: false,
-      maxZoom: 15,
-      minZoom: 5,
-    };
+    }
   }
-
-  handleChange(event) {
+  
+  handleChange(event){
     this.props.onChange(event);
   }
 
   render() {
+
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: "100%", width: "100%" }}>
-        <LoaderMapSpotFetch isFetching={this.props.isFetching} />
         <GoogleMapReact
-          onMarkerClick={this.props.onMarkerClick}
           bootstrapURLKeys={{ key: "AIzaSyAddqyJJRGL9vCNBS8IARse91gKkEhMgYM" }}
           defaultCenter={this.validateCenter()}
           defaultZoom={this.props.zoom}
           options={this.createMapOptions}
           onChange={this.handleChange}
           onTilesLoaded={this.alertLoaded}
-          resetBoundsOnResize={true}
         >
-          {this.loadMarkers(this.props.markerSelected)}
+          
+          {this.loadMarkers()}
+      
         </GoogleMapReact>
       </div>
     );
